219922779ab1681b985be3641de41fb9b3709a3e
aoyore <pannarat.sh@mail.kmutt.ac.th>
2018-11-11 13:48:48 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>ข้างล่างคือรูป</h1>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" firstName="aoy"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
